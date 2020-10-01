81f2122d6664af7ad9e2d160147ad16f65931a03
Jeffrey Smith <jsmith@gasbuddy.com>
2020-10-01 13:43:53 -0400

    fix: Export useDimensions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as useAfterEffect } from './useAfterEffect';
+export { default as useDimensions } from './useDimensions';
 export { default as useIntersection } from './useIntersection';
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useResize } from './useResize';
